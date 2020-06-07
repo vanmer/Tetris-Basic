56210237fca09b7ef436ac8284ca80df5f904f3b
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-07 03:11:34 +0200

    commit j- and s-tetrominos

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
 
+  // the Tetrominoes
+  const jTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const sTetromino = [
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
+  ]
+  
+  const tTetromino = [
+
+  ]
 
 
 })
