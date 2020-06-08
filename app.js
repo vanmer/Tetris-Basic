c8c899008d49463234f362553ace10ca30a17c08
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-09 01:40:46 +0200

    commit rotate function

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (e.keyCode === 37) {
       moveLeft();
     } else if (e.keyCode === 38) {
-      // rotate
+      rotate();
     } else if (e.keyCode === 39) {
       moveRight();
     } else if (e.keyCode === 40) {
@@ -141,6 +141,17 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition -= 1;
     }
 
+    draw();
+  }
+
+  // rotate the Tetromino
+  function rotate() {
+    undraw();
+    currentRotation ++;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
     draw();
   }
 
