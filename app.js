70f6e0a8bce918e71cada8f6e9e9c93b48a2cbf5
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-09 00:14:45 +0200

    new commit -adding move down function and freeze function

--- app.js
+++ app.js
@@ -64,15 +64,43 @@ document.addEventListener('DOMContentLoaded', () => {
   let random = Math.floor(Math.random() * theTetrominoes.length);
   let current = theTetrominoes[random][currentRotation];
 
-  // draw the Tetro
+  // draw the Tetromino
   function draw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino');
     })
+  };
+
+  // undraw the Tetromino
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino');
+    })
+  };
+
+  // make the Tetromino move down every second
+  timerId = setInterval(moveDown, 1000);
+
+  // move down function
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  };
+
+  // freeze function
+  function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+      // start a new Tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
   }
 
-  // draw();
-  
-  //
+
 
 })
