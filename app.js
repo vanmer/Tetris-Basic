410b7d9560804462e24b1f0113f98711b8e1dc80
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-08 23:32:21 +0200

    another commit, generating tetrominoes

--- app.js
+++ app.js
@@ -11,18 +11,68 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width+1, width+2, width*2+2],
     [1, width+1, width*2+1, width*2],
     [width, width*2, width*2+1, width*2+2]
-  ]
+  ];
 
   const sTetromino = [
     [0, width, width+1, width*2+1],
     [width+1, width+2, width*2, width*2+1],
     [0, width, width+1, width*2+1],
     [width+1, width+2, width*2, width*2+1]
-  ]
-  
+  ];
+
   const tTetromino = [
+    [1, width, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
+  ];
 
-  ]
+  const oTetromino = [
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
+  ];
 
+  const iTetromino = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+  ];
+
+  const lTetromino = [
+    [0, 1, width+1, width*2+1],
+    [width, width+1, width+2, width*2],
+    [1, width+1, width*2+1, width*2+2],
+    [2, width, width+1, width+2]
+  ];
+
+  const zTetromino = [
+    [width, width+1, width*2+1, width*2+2],
+    [1, width, width+1, width*2],
+    [width, width+1, width*2+1, width*2+2],
+    [1, width, width+1, width*2]
+  ];
+
+  const theTetrominoes = [ jTetromino, sTetromino, tTetromino, oTetromino, iTetromino, lTetromino, zTetromino ];
+
+  let currentPosition = 4;
+  let currentRotation = 0;
+
+  // randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let current = theTetrominoes[random][currentRotation];
+
+  // draw the Tetro
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino');
+    })
+  }
+
+  // draw();
+  
+  //
 
 })
