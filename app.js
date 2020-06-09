9b655be2495237aead50364c097582de3ecc6a4e
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-09 10:21:03 +0200

    commit, add score functionality

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10;
   let nextRandom = 0;
   let timerId;
+  let score = 0;
 
   // the Tetrominoes
   const jTetromino = [
@@ -117,6 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4;
       draw();
       displayShape();
+      addScore();
     }
   }
 
@@ -201,7 +203,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 
-  
+  // add score
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9];
+
+      if (row.every(index => squares[index].classList.contains('taken'))) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+        row.forEach(index => {
+          squares[index].classList.remove('taken');
+        })
+        const squaresRemoved = squares.splice(i, width);
+        console.log(squaresRemoved);
+      }
+    }
+  }
 
 
 
