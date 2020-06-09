37ac3082f8611c78fe01e63d47ecd164837fc1c3
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-09 10:28:27 +0200

    commit, squares removed in row

--- app.js
+++ app.js
@@ -213,9 +213,11 @@ document.addEventListener('DOMContentLoaded', () => {
         scoreDisplay.innerHTML = score;
         row.forEach(index => {
           squares[index].classList.remove('taken');
+          squares[index].classList.remove('tetromino');
         })
         const squaresRemoved = squares.splice(i, width);
-        console.log(squaresRemoved);
+        squares = squaresRemoved.concat(squares);
+        squares.forEach(cell => grid.appendChild(cell));
       }
     }
   }
